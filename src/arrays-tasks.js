c66bd8b7d909b25da68979154ec1dcbcf3b841c0
Simonas <s.zilevicius@gmail.com>
2025-01-12 14:16:53 +0200

    feat: more tasks solved

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -142,8 +142,9 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  const itemLength = arr[0].length;
+  return arr.every((a) => a.length === itemLength);
 }
 
 /**
@@ -157,8 +158,8 @@ function isSameLength(/* arr */) {
  *    isValueEqualsIndex([2, 1, 0, 4, 5]) => true
  *    isValueEqualsIndex([10, 20, 30, 40, 50]) => false
  */
-function isValueEqualsIndex(/* arr */) {
-  throw new Error('Not implemented');
+function isValueEqualsIndex(arr) {
+  return arr.some((a) => a === arr[a]);
 }
 
 /**
